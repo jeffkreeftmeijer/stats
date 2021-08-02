34e67c2641d5ae5c5c68c386d08e152fab76b41d
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2021-08-02 17:03:21 +0200

    Add CSV output

---
+++ csv.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+find ~/stats/logs -name "*.gz" | \
+    xargs gzcat | \
+    /usr/local/bin/goaccess \
+	--log-format CLOUDFRONT \
+	--date-format CLOUDFRONT \
+	--time-format CLOUDFRONT \
+	--ignore-crawlers \
+	--ignore-status=301 \
+	--ignore-status=302 \
+	--keep-last=28 \
+	--output ~/stats/stats.csv
