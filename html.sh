f8fb2ab217110013d030b45813d8d5c35389eb99
Jeff Kreeftmeijer <jeff@kreeft.me>
2023-04-26 21:31:25 +0200

    Update log format to fix isses on newer GoAccess version

--- html.sh
+++ html.sh
@@ -4,7 +4,7 @@ find logs -name "*.gz" | \
     xargs gzcat | \
     grep --invert-match --file=exclude.txt | \
     /usr/local/bin/goaccess \
-	--log-format CLOUDFRONT \
+	--log-format "%d\\t%t\\t%^\\t%b\\t%h\\t%m\\t%^\\t%r\\t%s\\t%R\\t%u\\t%^" \
 	--date-format CLOUDFRONT \
 	--time-format CLOUDFRONT \
 	--ignore-crawlers \
